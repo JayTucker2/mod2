92fd1a0b291b435354fd3edf3fd65c814bbd41c9
JayTucker2 <prosnipa1221@gmail.com>
2021-09-03 07:40:21 -0500

    Initial Commit

---
+++ mod2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mod2
+//
+//  Created by JAYLAN TUCKER on 9/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
