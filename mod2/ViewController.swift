44133ff814fecca0594f78815ddec1009fab9fe3
Jaylan <prosnipa1221@gmail.com>
2021-09-07 06:52:20 -0500

    pre-safe unwrapping

--- mod2/ViewController.swift
+++ mod2/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var xTextField: UITextField!
+    @IBOutlet weak var yTextField: UITextField!
+    @IBOutlet weak var answer: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func pythButton(_ sender: Any) {
+        var x = xTextField.text
+        if let varx = Int(x){
+            
+        }else{
+            answer.text = "One or both of your values "
+        }
+        
+    }
+    
+    
+    
+    
 
 }
 
