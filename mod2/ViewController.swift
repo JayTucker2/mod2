d3117a08fd6b9e296d2434fe874dbdc02d27f84d
JayTucker2 <prosnipa1221@gmail.com>
2021-09-10 08:08:46 -0500

    safe unwrap

--- mod2/ViewController.swift
+++ mod2/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var xTextField: UITextField!
     @IBOutlet weak var yTextField: UITextField!
     @IBOutlet weak var answer: UILabel!
@@ -16,12 +16,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        xTextField.delegate = self
+        yTextField.delegate = self
         // Do any additional setup after loading the view.
         
     }
+    @IBAction func rectButton(_ sender: Any) {
+    
+    }
     @IBAction func pythButton(_ sender: Any) {
-      
+        let pth1 = Int(xTextField.text!) ?? 0
+        let pth2 = Int(yTextField.text!) ?? 0
+
+        
+        xTextField.resignFirstResponder()
+        yTextField.resignFirstResponder()
         }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        xTextField.resignFirstResponder()
+        yTextField.resignFirstResponder()
+        return true
+    }
         
     }
     
